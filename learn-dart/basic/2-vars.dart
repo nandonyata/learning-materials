100877d6ac58761c9b6ca31f6dea9eae1cd4a67c
nandonyata <nandonp02@gmail.com>
2026-07-09 19:41:18 +0700

    feat: vars

---
+++ learn-dart/basic/2-vars.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Declare a variable
+
+  var name = 'John Doe';
+  String name2 = 'Jane Doe';
+  print('Name: $name');
+  print('Name2: $name2');
+
+  var age = 24;
+  int age2 = 25;
+  print('Age: $age');
+  print('Age2: $age2');
+}
